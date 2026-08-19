53ab8dadadb438072d11ab17d5c248b8daa215c2
1225100336sma-ux <1225100336.sma@gmail.com>
2026-08-19 00:22:04 -0600

    NuevoPrestamoController
    Se hizo una corrección en los horarios de devolución, se agregaron más horas de las previamente establecidas

--- src/main/java/utng/gtid2/dab/controllers/prestamos/NuevoPrestamoController.java
+++ src/main/java/utng/gtid2/dab/controllers/prestamos/NuevoPrestamoController.java
@@ -78,20 +78,22 @@ public class NuevoPrestamoController implements Initializable {
 
         //================ HORARIOS =================
 
-        cmbHoraDevolucion.getItems().addAll(
-                "08:00",
-                "09:00",
-                "10:00",
-                "11:00",
-                "12:00",
-                "13:00",
-                "14:00",
-                "15:00",
-                "16:00",
-                "17:00");
+cmbHoraDevolucion.getItems().addAll(
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00");
 
-        cmbHoraDevolucion.getSelectionModel().selectFirst();
-
+cmbHoraDevolucion.getSelectionModel().selectFirst();
         //================ CARGAR MATERIALES =================
 
         cmbMaterial.getItems().clear();
